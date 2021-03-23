a8da7ba2167db3bc07dcb6f54e63191a2a911277
iLLDopamine <InfernalLight@yandex.ru>
2021-03-23 23:43:42 +0300

    Content added to dialogs along with styles in it...

--- src/components/Dialogos/Dialogs.jsx
+++ src/components/Dialogos/Dialogs.jsx
@@ -2,8 +2,30 @@ import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className = {classes.dialogs}>
+            <div className = {classes.dialogsItems}>
+                <div className = {`${classes.dialog} ${classes.active}`}>
+                    Kirill
+                </div>
+
+                <div className = {classes.dialog}>
+                    Jenek
+                </div>
+
+                <div className = {classes.dialog}>
+                    Diman
+                </div>
+
+                <div className = {classes.dialog}>
+                    Danila
+                </div>
+            </div>
+
+            <div className = {classes.messages}>
+                <div className = {classes.message}>Hi!</div>
+                <div className = {classes.message}>How are you?</div>
+                <div className = {classes.message}>Yo!</div>
+            </div>
         </div>
     );
 }
