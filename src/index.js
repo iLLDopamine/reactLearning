0932a4f37d0e779bbf12596ea5fa282fe86edc39
iLLDopamine <InfernalLight@yandex.ru>
2021-03-31 00:34:37 +0300

    Created copy Profile Reducer

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 
-let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
       <BrowserRouter>
@@ -18,13 +17,8 @@ let rerenderEntireTree = (state) => {
     </React.StrictMode>,
     document.getElementById('root')
   );
-}
 
-rerenderEntireTree();
 
-store.subscribe(() => {
-  rerenderEntireTree();
-});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
