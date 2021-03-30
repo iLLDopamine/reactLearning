648882f33a775169b50f9fc6f892beeee909a2f5
iLLDopamine <InfernalLight@yandex.ru>
2021-03-31 02:30:21 +0300

    Fixed bug in profile reducer and refactored dialogs reducer

--- src/components/Dialogos/Dialogs.jsx
+++ src/components/Dialogos/Dialogs.jsx
@@ -9,8 +9,8 @@ const Dialogs = (props) => {
 
     let state = props.dialogsPage;
 
-    let dialogsElements = state.dialogsData.map( dialog => <DialogItem name = { dialog.name } id = { dialog.id } /> );
-    let messagesElements = state.messagesData.map( message => <Message message = { message.message } id = { message.id } /> );
+    let dialogsElements = state.dialogsData.map( dialog => <DialogItem name = { dialog.name } key = { dialog.id } id = { dialog.id } /> );
+    let messagesElements = state.messagesData.map( message => <Message message = { message.message } key = { message.id } id = { message.id } /> );
     let newMessageBody = state.newMessageBody;
     
     let onSendMessageClick = () => {
